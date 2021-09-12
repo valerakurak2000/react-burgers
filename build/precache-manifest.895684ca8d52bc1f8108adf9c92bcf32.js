ded3e51262b63f0e718e8084fde36af5698a2189
valerakurak2000 <50573695+valerakurak2000@users.noreply.github.com>
2021-09-12 08:20:48 +0300

    Build Commit

---
+++ build/precache-manifest.895684ca8d52bc1f8108adf9c92bcf32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2f499faa6eef656ca0e827d05d4c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e033ea0c0bce5aa797",
+    "url": "/static/css/main.c9fb760e.chunk.css"
+  },
+  {
+    "revision": "57962469a27c4d5e6a4c",
+    "url": "/static/js/2.618b884e.chunk.js"
+  },
+  {
+    "revision": "80e033ea0c0bce5aa797",
+    "url": "/static/js/main.6da5747b.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
